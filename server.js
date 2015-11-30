89c204cd51394a0299a528aa314a76e617c901d8
Kunal Saddi <kunalsaddi@gmail.com>
2015-11-30 02:20:56 -0500

    Set up working environment for tic-tac-toe

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var morgan = require('morgan');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+//app.use(morgan('combined'));  //for logging
+app.use(serveStatic(__dirname + '/public'));
+
+var port = 3000;
+app.set('port', process.env.PORT || port);
+
+var server = app.listen(app.get('port'), function () {
+  console.log('Static server listening on port %s', server.address().port);
+});
