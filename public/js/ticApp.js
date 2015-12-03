eeab82d303abd757c438e95ab35e46336c7076db
king-saddi <kunalsaddi@gmail.com>
2015-12-03 18:35:22 -0500

    Added image response and winning logic

--- public/js/ticApp.js
+++ public/js/ticApp.js
@@ -2,45 +2,67 @@
 angular.module('ticApp', [])
   .controller('ticController', [function() {
       var tic = this;
+      tic.moves = 0;
       tic.player1 = "X";
       tic.player2 = "O";
       tic.currentPlayer = "";
-      tic.board = [
-          {position: 0, value:""},
-          {position: 1, value:""},
-          {position: 2, value:""},
-          {position: 3, value:""},
-          {position: 4, value:""},
-          {position: 5, value:""},
-          {position: 6, value:""},
-          {position: 7, value:""},
-          {position: 8, value:""}
-          ];
+      tic.board = {
+          0:"",
+          1:"",
+          2:"",
+          3:"",
+          4:"",
+          5:"",
+          6:"",
+          7:"",
+          8:""
+      };
           
-          
-          
-      
-      
+
       tic.start = function(){
           console.log('Started tic-tac-toe game.');
           tic.currentPlayer = tic.player1;
           console.log(tic.currentPlayer);
       };
       
-      tic.select = function(id){
+      
+      
+      tic.gameOver = function(){
           
-          if (tic.currentPlayer == tic.player1){
-              console.log(tic.currentplayer);
-              tic.currentPlayer = tic.player2;
+          if (
+              ((tic.board[0] == tic.board[1]) && (tic.board[0] == tic.board[2]) && tic.board[0] != "") ||
+              ((tic.board[3] == tic.board[4]) && (tic.board[3] == tic.board[5]) && tic.board[3] != "") ||
+              ((tic.board[6] == tic.board[7]) && (tic.board[6] == tic.board[8]) && tic.board[6] != "")
+             )
+              {
+             alert("GAME OVER!!"); 
+              }
+      };
+      
+      tic.select = function(loc){
+          
+          if(tic.board[loc] != ""){
+              alert("Please choose a different position!");
+              
           }
           else{
-              console.log(tic.currentPlayer);
-              tic.currentPlayer = tic.player1;
-          }
-          
-          
+              tic.moves++;
+              tic.board[loc] = tic.currentPlayer;
+
+              
+              if (tic.currentPlayer == tic.player1){
+                  tic.currentPlayer = tic.player2;
+              }
+              else{
+                  tic.currentPlayer = tic.player1;
+              }
+              
+              tic.gameOver();
+              
+              }
       };
-  
+    
+
   }]);
 
 
