9c2f36cb48198e5757ace79b16440f07b72af6a5
kms6242 <kms6242@psu.edu>
2015-12-03 20:44:56 -0800

    Added the rest of the logic for a winning game (vert and diagonal), added functionality to start a new game and reset the board, cleaned up indentation.

--- public/js/ticApp.js
+++ public/js/ticApp.js
@@ -1,69 +1,85 @@
-
 angular.module('ticApp', [])
-  .controller('ticController', [function() {
-      var tic = this;
-      tic.moves = 0;
-      tic.player1 = "X";
-      tic.player2 = "O";
-      tic.currentPlayer = "";
-      tic.board = {
-          0:"",
-          1:"",
-          2:"",
-          3:"",
-          4:"",
-          5:"",
-          6:"",
-          7:"",
-          8:""
-      };
-          
+	.controller('ticController', [function() {
+		var tic = this;
+		tic.moves = 0;
+		tic.over = false;
+		tic.player1 = "X";
+		tic.player2 = "O";
+		tic.currentPlayer = "";
+		tic.board = {
+			0:"",
+			1:"",
+			2:"",
+			3:"",
+			4:"",
+			5:"",
+			6:"",
+			7:"",
+			8:""
+		};
 
-      tic.start = function(){
-          console.log('Started tic-tac-toe game.');
-          tic.currentPlayer = tic.player1;
-          console.log(tic.currentPlayer);
-      };
-      
-      
-      
-      tic.gameOver = function(){
-          
-          if (
-              ((tic.board[0] == tic.board[1]) && (tic.board[0] == tic.board[2]) && tic.board[0] != "") ||
-              ((tic.board[3] == tic.board[4]) && (tic.board[3] == tic.board[5]) && tic.board[3] != "") ||
-              ((tic.board[6] == tic.board[7]) && (tic.board[6] == tic.board[8]) && tic.board[6] != "")
-             )
-              {
-             alert("GAME OVER!!"); 
-              }
-      };
-      
-      tic.select = function(loc){
-          
-          if(tic.board[loc] != ""){
-              alert("Please choose a different position!");
-              
-          }
-          else{
-              tic.moves++;
-              tic.board[loc] = tic.currentPlayer;
+		resetBoard = function() {
+			tic.board = {
+			0:"",
+			1:"",
+			2:"",
+			3:"",
+			4:"",
+			5:"",
+			6:"",
+			7:"",
+			8:""
+			};
+		}
 
-              
-              if (tic.currentPlayer == tic.player1){
-                  tic.currentPlayer = tic.player2;
-              }
-              else{
-                  tic.currentPlayer = tic.player1;
-              }
-              
-              tic.gameOver();
-              
-              }
-      };
-    
+		tic.start = function(){
+			console.log('Started tic-tac-toe game.');
+			tic.currentPlayer = tic.player1;
+			resetBoard();
+			tic.over = false;
+		};
 
-  }]);
+		tic.checkForWinner = function(){
+			// horizontal wins
+			if (((tic.board[0] == tic.board[1] && tic.board[1] == tic.board[2]) && tic.board[0] != "") || 
+			((tic.board[3] == tic.board[4] && tic.board[4] == tic.board[5]) && tic.board[3] != "") || 
+			((tic.board[6] == tic.board[7] && tic.board[7] == tic.board[8]) && tic.board[6] != "") || 
+			// vertical wins
+			((tic.board[0] == tic.board[3] && tic.board[3] == tic.board[6]) && tic.board[0] != "") || 
+			((tic.board[1] == tic.board[4] && tic.board[4] == tic.board[7]) && tic.board[1] != "") || 
+			((tic.board[2] == tic.board[5] && tic.board[5] == tic.board[8]) && tic.board[2] != "") || 
+			// diagonal wins
+			((tic.board[0] == tic.board[4] && tic.board[4] == tic.board[8]) && tic.board[0] != "") || 
+			((tic.board[2] == tic.board[4] && tic.board[4] == tic.board[6]) && tic.board[2] != "")) {
+				tic.over = true;
+			}
+		}
+		
+		tic.endGame = function() {
+			alert("The game is over, click start to play again!");
+		}
+		
+		tic.select = function(loc) {
+			if(tic.over) {
+				tic.endGame();
+			}
+			else if(tic.board[loc] != "") {
+				alert("Please choose a different position!");
+			}
+			else {
+				tic.moves++;
+				tic.board[loc] = tic.currentPlayer;
+
+				if (tic.currentPlayer == tic.player1) {
+					tic.currentPlayer = tic.player2;
+				}
+				else {
+					tic.currentPlayer = tic.player1;
+				}
+				tic.checkForWinner();
+			}
+		};
+	}]);
 
 
 
